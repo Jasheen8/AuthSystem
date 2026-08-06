e467ea74ff530ed59bc048b0ffcd783c1a69c468
delta student <jasheenansari8@gmail.com>
2026-08-06 11:39:50 +0530

    fixing email error

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,67 +1,69 @@
-// const nodemailer = require("nodemailer");
-
-// const sendEmail = async (
-//   email,
-//   subject,
-//   message
-// ) => {
-//   try {
-//     const transporter =
-//       nodemailer.createTransport({
-//         service: "gmail",
-//         auth: {
-//           user: process.env.EMAIL_USER,
-//           pass: process.env.EMAIL_PASS,
-//         },
-//       });
-
-//     console.log("Verifying transporter...");
-
-//     await transporter.verify();
-
-//     console.log("Transport verified!");
-
-//     const info =
-//       await transporter.sendMail({
-//         from: process.env.EMAIL_USER,
-//         to: email,
-//         subject,
-//         html: message,
-//       });
-
-//     console.log("Email sent!");
-//     console.log(info);
-
-//   } catch (error) {
-//     console.error("EMAIL ERROR:");
-//     console.error(error);
-//     throw error;
-//   }
-// };
-
-// module.exports = sendEmail;
 const nodemailer = require("nodemailer");
 
-const sendEmail = async (email, subject, message) => {
-  const transporter = nodemailer.createTransport({
-    service: "smtp.gmail.com",
+const sendEmail = async (
+  email,
+  subject,
+  message
+) => {
+  try {
+    const transporter =
+      nodemailer.createTransport({
+        service: "smtp.gmail.com",
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+      });
 
-    auth: {
-      user: process.env.EMAIL_USER,
+    console.log("Verifying transporter...");
 
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+    await transporter.verify();
 
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
+    console.log("Transport verified!");
 
-    to: email,
+    const info =
+      await transporter.sendMail({
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject,
+        html: message,
+      });
 
-    subject,
+    console.log("Email sent!");
+    console.log(info);
 
-    html: message,
-  });
+  } catch (error) {
+    console.error("EMAIL ERROR:");
+    console.error(error);
+    throw error;
+  }
 };
 
 module.exports = sendEmail;
+
+
+// const nodemailer = require("nodemailer");
+
+// const sendEmail = async (email, subject, message) => {
+//   const transporter = nodemailer.createTransport({
+//     service: "smtp.gmail.com",
+
+//     auth: {
+//       user: process.env.EMAIL_USER,
+
+//       pass: process.env.EMAIL_PASS,
+//     },
+//   });
+
+//   await transporter.sendMail({
+//     from: process.env.EMAIL_USER,
+
+//     to: email,
+
+//     subject,
+
+//     html: message,
+//   });
+// };
+
+// module.exports = sendEmail;
